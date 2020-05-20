9652f86866b6537ce81f9f4881945a899f5f803a
Theodore Tan <theo@kasa.com>
2020-05-20 12:19:46 -0700

    Updates

---
+++ precache-manifest.9ffcacd1fc11db625c8fbbfd361f9bfb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fc248c0235953596ca604505662af397",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6ad47338eeb1cef578a0",
+    "url": "/static/css/main.692d75bb.chunk.css"
+  },
+  {
+    "revision": "af7fce1acb9f74fa399d",
+    "url": "/static/js/2.38dd8650.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.38dd8650.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6ad47338eeb1cef578a0",
+    "url": "/static/js/main.911ca1a2.chunk.js"
+  },
+  {
+    "revision": "590e9f4cc22c98a9cac0",
+    "url": "/static/js/runtime-main.f852fa46.js"
+  },
+  {
+    "revision": "dc0c7254a69581bbc4b3eff9439a7ad4",
+    "url": "/static/media/pik_cropped.dc0c7254.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.bfd177dd602ded5de3bf2554d4e4a7b2.js"
+  "/precache-manifest.9ffcacd1fc11db625c8fbbfd361f9bfb.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
