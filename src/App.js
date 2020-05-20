ef1cded00ffb648d6ef609015de0db929da19e4b
Theodore Tan <theodoresinhantan@gmail.com>
2020-05-20 12:50:06 -0700

    adjust test script for ci/cd

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>Theo&apos;s website in construction... TEST123</p>
+        <p>Theo&apos;s website in construction!</p>
       </header>
     </div>
   );
